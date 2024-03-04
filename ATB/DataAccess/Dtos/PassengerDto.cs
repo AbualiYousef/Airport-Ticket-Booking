96f0fd8b44b897a36a0b516b146e5b5352b535d4
Yousef Abuali <abualiyousef@outlook.com>
2024-03-04 15:36:54 +0200

    Add Dtos

---
+++ ATB/DataAccess/Dtos/PassengerDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Dtos;
+
+public class PassengerDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string PassportNumber { get; set; }
+} //End of PassengerDto 
