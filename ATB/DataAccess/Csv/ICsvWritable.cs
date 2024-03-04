edfbce823c118cc41575a4f7a24d9481483df46e
Yousef Abuali <abualiyousef@outlook.com>
2024-03-04 15:35:42 +0200

    Refactor on models

--- ATB/DataAccess/Csv/ICsvWritable.cs
+++
@@ -1,8 +0,0 @@
-namespace DataAccess.Csv;
-
-public interface ICsvWritable
-{
-    string GetCsvRecord();
-
-    static abstract string GetHeader();
-}
--- ATB/DataAccess/Models/Passenger.cs
+++ ATB/DataAccess/Models/Passenger.cs
@@ -5,4 +5,10 @@ public class Passenger
     public int Id { get; init; }
 
     public string Name { get; init; }
+    
+    public string Email { get; init; }
+    
+    public string PhoneNumber { get; init; }
+    
+    public string PassportNumber { get; set; }
 } //End of Passenger class
