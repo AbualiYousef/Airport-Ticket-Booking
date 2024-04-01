7009617d9df5cda82845c338feba645db516b9cb
Yousef Abuali <abualiyousef@outlook.com>
2024-04-01 03:30:30 +0200

    Implement some tests for 'CsvFileService'

---
+++ ATB/DataAccess.Tests/CsvFileServiceTests.cs
@@ -0,0 +1,98 @@
+using DataAccess.CsvHelperService;
+using FluentAssertions;
+
+namespace ATB.Tests;
+
+public class CsvFileServiceTests
+{
+    #region ReadFromCsvAsync
+
+    [Fact]
+    public async Task ReadFromCsvAsync_NonExistentFile_ShouldThrowException()
+    {
+        var csvFileService = new CsvFileService<TestData>();
+        var nonExistentFilePath = Path.Combine(Path.GetTempPath(), "nonexistent_file.csv");
+        await Assert.ThrowsAsync<FileNotFoundException>(async () =>
+            await csvFileService.ReadFromCsvAsync(nonExistentFilePath));
+    }
+
+    [Fact]
+    public async Task ReadFromCsvAsync_EmptyFile_ShouldReturnEmptyList()
+    {
+        var tempFilePath = Path.GetTempFileName();
+        var csvFileService = new CsvFileService<TestData>();
+
+        var result = await csvFileService.ReadFromCsvAsync(tempFilePath);
+
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task ReadFromCsvAsync_ValidData_ShouldReturnCorrectData()
+    {
+        var tempFilePath = Path.GetTempFileName();
+        await File.WriteAllLinesAsync(tempFilePath,
+            new[] { "Id,Name", "1,Test 1", "2,Test 2" });
+        var csvFileService = new CsvFileService<TestData>();
+
+        var result = await csvFileService.ReadFromCsvAsync(tempFilePath);
+
+        var expected = new List<TestData>
+        {
+            new TestData { Id = 1, Name = "Test 1" },
+            new TestData { Id = 2, Name = "Test 2" }
+        };
+        result.Should().BeEquivalentTo(expected);
+        File.Delete(tempFilePath);
+    }
+
+    #endregion
+
+    #region WriteToCsvAsync
+
+    [Fact]
+    public async Task WriteToCsvAsync_ValidData_ShouldWriteCorrectData()
+    {
+        var tempFilePath = Path.GetTempFileName();
+        var csvFileService = new CsvFileService<TestData>();
+        var data = new List<TestData>
+        {
+            new TestData { Id = 1, Name = "Test 1" },
+            new TestData { Id = 2, Name = "Test 2" }
+        };
+
+        await csvFileService.WriteToCsvAsync(tempFilePath, data);
+
+        var result = await csvFileService.ReadFromCsvAsync(tempFilePath);
+        result.Should().BeEquivalentTo(data);
+        File.Delete(tempFilePath);
+    }
+
+    [Fact]
+    public async Task WriteToCsvAsync_EmptyData_ShouldWriteEmptyFile()
+    {
+        var tempFilePath = Path.GetTempFileName();
+        var csvFileService = new CsvFileService<TestData>();
+        var data = new List<TestData>();
+
+        await csvFileService.WriteToCsvAsync(tempFilePath, data);
+
+        var result = await csvFileService.ReadFromCsvAsync(tempFilePath);
+        result.Should().BeEmpty();
+        File.Delete(tempFilePath);
+    }
+
+    [Fact]
+    public async Task WriteToCsvAsync_NonExistentDirectory_ShouldThrowException()
+    {
+        var csvFileService = new CsvFileService<TestData>();
+        var nonExistentDirectoryPath = Path.Combine(Path.GetTempPath(), "nonexistent_directory");
+        var nonExistentFilePath = Path.Combine(nonExistentDirectoryPath, "nonexistent_file.csv");
+        var data = new List<TestData>();
+
+        await Assert.ThrowsAsync<DirectoryNotFoundException>(async () =>
+            await csvFileService.WriteToCsvAsync(nonExistentFilePath, data));
+    }
+
+    #endregion
+}
